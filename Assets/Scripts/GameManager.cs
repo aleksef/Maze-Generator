4f2d4265203ea3c303f3ce35b32c78a010672e9d
aleksef <38755134+aleksef@users.noreply.github.com>
2022-08-17 22:41:58 +0300

    add grid generation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> grid;
+    [SerializeField] GameObject nodePrefab;
+    [SerializeField] GameObject wallPrefab;
+    [SerializeField] Slider speedSlider;
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject ground;
+
+    private void Start()
     {
-        
+        speedSlider.onValueChanged.AddListener(delegate { ChangeSpeed(speedSlider.value); });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateGrid() 
     {
-        
+        CreateGridCells(5, 5);
+    }
+
+    private void CreateGridCells(int lengthX, int lengthZ)
+    {
+        mainCamera.transform.position = new(lengthX - 1, mainCamera.transform.position.y, lengthZ + 1);
+        ground.transform.position = new(lengthX - 1, 0, lengthZ + 1);
+
+        for (int z = lengthZ; z > 0; z--)
+        {
+            for (int x = 0; x < lengthX; x++)
+            {
+                InstanciateGridCell(nodePrefab,
+                        new(x * 2, 0, z * 2)
+                        );
+                if (x != 4) 
+                {
+                    InstanciateGridCell(wallPrefab,
+                        new(x * 2 + 1, 0, z * 2)
+                        );
+                }
+            }
+            if (z > 1) 
+            {
+                // instantiate wall row
+                for (int x = 0; x < lengthX * 2 - 1; x++)
+                {
+                    InstanciateGridCell(wallPrefab,
+                        new(x, 0, z * 2 - 1)
+                        );
+                }
+            }
+        }
+    }
+
+    private void InstanciateGridCell(GameObject prefab, Vector3 position) 
+    {
+        GameObject newObj = Instantiate(prefab, position, prefab.transform.rotation);
+        newObj.GetComponent<GridCell>().gridX = (int)position.x;
+        newObj.GetComponent<GridCell>().gridZ = (int)position.z;
+        grid.Add(newObj);
+    }
+
+    public void ChangeSpeed(float scale) 
+    {
+        Time.timeScale = scale;
     }
 }
---
+++ Assets/Scripts/GridCell.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell : MonoBehaviour
+{
+    public int gridX;
+    public int gridZ;
+    public bool isVisited = false;
+}
