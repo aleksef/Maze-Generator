561153622df5ea7115e2b0e55f25475b4155ec1e
aleksef <38755134+aleksef@users.noreply.github.com>
2022-08-19 00:21:51 +0300

    remove node script and add it to gridCell script

--- Assets/Scripts/GridCell.cs
+++ Assets/Scripts/GridCell.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class GridCell : MonoBehaviour
 {
+    [SerializeField] Material currentMaterial;
+    [SerializeField] Material visitedMaterial;
     public int gridX;
     public int gridZ;
+    public bool isVisited = false;
+
+    public void SetCurrent()
+    {
+        gameObject.GetComponent<Renderer>().material = currentMaterial;
+        isVisited = true;
+    }
+
+    public void SetVisited()
+    {
+        gameObject.GetComponent<Renderer>().material = visitedMaterial;
+        isVisited = true;
+    }
 }
